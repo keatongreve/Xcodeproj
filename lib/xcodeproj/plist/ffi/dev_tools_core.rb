fb72c815162a66d21ff01d911f1846b4236ebe11
Keaton Greve <keatongreve@gmail.com>
2016-09-21 20:04:21 -0500

    Duplicate of https://github.com/CocoaPods/Xcodeproj/pull/404

--- lib/xcodeproj/plist/ffi/dev_tools_core.rb
+++ lib/xcodeproj/plist/ffi/dev_tools_core.rb
@@ -1,15 +1,48 @@
 require 'fiddle'
 
+# Since Xcode 8 beta 4, calling `PBXProject.projectWithFile` does break subsequent calls to
+# `chdir`. While sounding ridiculous, this is unfortunately true and debugging it from the
+# userland side showed no difference at all to successful calls to `chdir`, but the working
+# directory is simply not changed in the end. This workaround is even more absurd, monkey
+# patching all calls to `chdir` to use `__pthread_chdir` which does appear to work just fine.
+class Dir
+  def self.chdir(path)
+    old_dir = Dir.getwd
+    res = actually_chdir(path)
+
+    if block_given?
+      begin
+        return yield
+      ensure
+        actually_chdir(old_dir)
+      end
+    end
+
+    res
+  end
+
+  private
+
+  def self.actually_chdir(path)
+    libc = Fiddle.dlopen '/usr/lib/libc.dylib'
+    f = Fiddle::Function.new(libc['__pthread_chdir'], [Fiddle::TYPE_VOIDP], Fiddle::TYPE_INT)
+    f.call(path.to_s)
+  end
+end
+
 module Xcodeproj
   module Plist
     module FFI
       module DevToolsCore
         def self.silence_stderr
-          orig_stderr = $stderr.clone
-          $stderr.reopen File.new('/dev/null', 'w')
-          yield
-        ensure
-          $stderr.reopen orig_stderr
+          begin
+            orig_stderr = $stderr.clone
+            $stderr.reopen File.new('/dev/null', 'w')
+            retval = yield
+          ensure
+            $stderr.reopen orig_stderr
+          end
+          retval
         end
 
         # rubocop:disable Style/MethodName
